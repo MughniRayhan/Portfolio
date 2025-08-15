5fbad6c581cca0b1de3d8a16f257fbffb5690625
MughniRayhan <tishtisha13@gmail.com>
2025-08-15 14:47:56 +0600

    add wave in banner

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactTypingEffect from 'react-typing-effect'
 import Profile from '../../assets/my-profile.jpg'
 import resume from '../../assets/resume.pdf'
+import { motion } from "framer-motion"
 
 function Banner() {
   return (
-    <div className='py-24 px-20  '>
+    <div className='py-24  px-20 '>
         <div className='flex flex-col-reverse md:flex-row justify-between items-center '>
           
            {/* left side */}
@@ -56,6 +57,22 @@ function Banner() {
             />
            </div>
         </div>
+<motion.svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='hidden lg:block' animate={{x:[0,20,0],y:[0,20,0]}}
+        transition={{duration:4, repeat:Infinity}} >
+  <defs>
+    {/* Gradient definition */}
+    <linearGradient id="waveGradient" x1="20%" y1="0%" x2="100%" y2="100%">
+      <stop offset="0%" style={{ stopColor: "#6c757d", stopOpacity: 1 }} />
+      <stop offset="47%" style={{ stopColor: "#0d1321", stopOpacity: 1 }} />
+      <stop offset="100%" style={{ stopColor: "#000814", stopOpacity: 1 }} />
+    </linearGradient>
+  </defs>
+
+  <path
+    fill="url(#waveGradient)"
+    d="M0,256L30,266.7C60,277,120,299,180,282.7C240,267,300,213,360,197.3C420,181,480,203,540,229.3C600,256,660,288,720,293.3C780,299,840,277,900,229.3C960,181,1020,107,1080,117.3C1140,128,1200,224,1260,234.7C1320,245,1380,171,1410,133.3L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
+  />
+</motion.svg>
     </div>
   )
 }
